11fd18a7f80954ba6c3d72a8d2a1e75c05bb0be4
Jorgemoraa <jorgemoratallavita@gmail.com>
2024-09-30 19:40:38 +0200

    crate class and create functions

---
+++ PlateVehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Practice1
+{
+	public class PlateVehicle : Vehicle
+	{
+
+		private string plate;
+		public PlateVehicle(string typeOfVehicle, string plate) : base(typeOfVehicle)
+		{
+			this.plate = plate;
+		}
+
+		public string GetPlate()
+		{
+			return plate;
+		}
+	}
+}
