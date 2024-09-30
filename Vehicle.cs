e1b02e792e83ccdf0388e4c4f267c05b7a3d5c1a
Jorgemoraa <jorgemoratallavita@gmail.com>
2024-09-30 17:10:46 +0200

    actualizacion vehiculos

--- Vehicle.cs
+++ Vehicle.cs
@@ -3,31 +3,31 @@
     abstract class Vehicle : IMessageWritter
     {
         private string typeOfVehicle;
-        private string plate;
+        //private string plate;
         private float speed;
 
         public Vehicle(string typeOfVehicle, string plate)
         {
             this.typeOfVehicle = typeOfVehicle;
-            this.plate = plate;
+            //this.plate = plate;
             speed = 0f;
         }
 
         //Override ToString() method with class information
-        public override string ToString()
-        {
-            return $"{GetTypeOfVehicle()} with plate {GetPlate()}";
-        }
+        //public override string ToString()
+        //{
+        //    return $"{GetTypeOfVehicle()} with plate {GetPlate()}";
+        //}
 
         public string GetTypeOfVehicle()
         {
             return typeOfVehicle;
         }
 
-        public string GetPlate()
-        {
-            return plate;
-        }
+        //public string GetPlate()
+        //{
+        //    return plate;
+        //}
 
 
         public float GetSpeed()
@@ -46,3 +46,4 @@
             return $"{this}: {message}";
         }
     }
+}
